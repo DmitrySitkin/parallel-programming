fe094cfe82fc29327a37a89ed77d99918e35dab4
DmitrySitkin <dimassitkin@gmail.com>
2018-12-21 13:52:07 +0300

    Update Task3.cpp

--- 1606-1/sitkin_da/task3/Task3.cpp
+++ 1606-1/sitkin_da/task3/Task3.cpp
@@ -10,28 +10,22 @@
 #define WEIGHT 5
 using namespace std;
 
-void print_d(int* d, int size) 
-{
-	for (int i = 0; i < size; i++) 
-	{
+void print_d(int* d, int size) {
+	for (int i = 0; i < size; i++) {
 		cout << d[i] << " ";
 	}
 	cout << endl;
 }
-void print_graph(int** G, int size)
-{
-	for (int i = 0; i < size; i++) 
-	{
-		for (int j = 0; j < size; j++)
-		{
+void print_graph(int** G, int size) {
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
 			cout << G[i][j] << " ";
 		}
 		cout << endl;
 	}
 }
 
-int* init_graph(int countEdge, int countVertex)
-{
+int* init_graph(int countEdge, int countVertex) {
 	int rank, procNum;
 
 	MPI_Comm_size(MPI_COMM_WORLD, &procNum);
@@ -40,25 +34,20 @@ int* init_graph(int countEdge, int countVertex)
 	int* one_graph = new int[countVertex * countVertex];
 	int** graph = new int*[countVertex];
 
-	for (int i = 0; i < countVertex; i++) 
-	{
+	for (int i = 0; i < countVertex; i++) {
 		graph[i] = new int[countVertex];
 	}
 	srand(time(NULL));
 
-	for (int i = 0; i < countVertex; i++) 
-	{
-		for (int j = 0; j < countVertex; j++)
-		{
-			if (i == j) 
-			{
+	for (int i = 0; i < countVertex; i++) {
+		for (int j = 0; j < countVertex; j++) {
+			if (i == j) {
 				graph[i][j] = 0;
+
 			}
-			else 
-			{
+			else {
 				graph[i][j] = rand() % 3;
-				if (graph[i][j] == 0) 
-				{
+				if (graph[i][j] == 0) {
 					graph[i][j] = INFINITI;
 				}
 			}
@@ -66,10 +55,8 @@ int* init_graph(int countEdge, int countVertex)
 		}
 		//cout << endl;
 	}
-	for (int i = 0, t = 0; i < countVertex; i++) 
-	{
-		for (int j = 0; j < countVertex; j++)
-		{
+	for (int i = 0, t = 0; i < countVertex; i++) {
+		for (int j = 0; j < countVertex; j++) {
 			one_graph[t] = graph[j][i];
 			t++;
 		}
@@ -77,34 +64,28 @@ int* init_graph(int countEdge, int countVertex)
 	return one_graph;
 }
 
-int* init_d(int size) 
-{
+int* init_d(int size) {
 	int* d = new int[size];
-	for (int i = 0; i < size; i++) 
-	{
+	for (int i = 0; i < size; i++) {
 		d[i] = INFINITI;
 	}
 	return d;
 }
 
-int* dijkstra(int* graph, int start, int count_vertex) 
-{
+int* dijkstra(int* graph, int start, int count_vertex) {
 	int* d = init_d(count_vertex);
 	d[start] = 0;
 	priority_queue<pair<int, int>>  queue;
 	queue.push(make_pair(0, start));
-	while (!queue.empty()) 
-	{
+	while (!queue.empty()) {
 		int v = queue.top().second, cur_d = queue.top().first;
 		queue.pop();
 
 		if (cur_d > d[v])  continue;
 
-		for (int i = 0; i < count_vertex; ++i) 
-		{
+		for (int i = 0; i < count_vertex; ++i) {
 			int to = i, len = graph[i * count_vertex + v];
-			if (d[v] + len < d[to]) 
-			{
+			if (d[v] + len < d[to]) {
 				d[to] = d[v] + len;
 				queue.push(make_pair(d[to], to));
 			}
@@ -113,23 +94,24 @@ int* dijkstra(int* graph, int start, int count_vertex)
 	return d;
 }
 
-int* parallel_dijkstra(int* graph, int start, int count_vertex) 
-{
+int* parallel_dijkstra(int* graph, int start, int count_vertex) {
 	int* d = nullptr;
+
 	int rank = 0, proc_num = 0;
 	MPI_Comm_size(MPI_COMM_WORLD, &proc_num);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
 	int* dist_d = new int[proc_num];
 	int* dist_graph = new int[proc_num];
 	int* count_element_d = new int[proc_num];
 	int* count_element_graph = new int[proc_num];
 	int part_size = count_vertex / (proc_num - 1);
-	count_element_d[0] = 0; 
+
+	count_element_d[0] = 0; //send to 0
 	dist_d[0] = 0;
 	count_element_graph[0] = 0;
 	dist_graph[0] = 0;
-	for (int i = 1; i < proc_num - 1; i++)
-	{
+	for (int i = 1; i < proc_num - 1; i++) {
 		count_element_d[i] = part_size;
 		dist_d[i] = (i - 1) * part_size;
 		count_element_graph[i] = count_vertex * part_size;
@@ -139,23 +121,26 @@ int* parallel_dijkstra(int* graph, int start, int count_vertex)
 	count_element_d[proc_num - 1] = count_vertex - (proc_num - 2) * part_size;
 	dist_graph[proc_num - 1] = (proc_num - 2) * (count_vertex * part_size);
 	count_element_graph[proc_num - 1] = count_vertex * count_vertex - (proc_num - 2) * (count_vertex*part_size);
+
 	int* part_graph = new int[count_element_graph[rank]];
 	int* part_d = new int[count_element_d[rank]];
-	int flag = 1, flag_finish_find_current_min_distation_in_sigment_of_rank = 1;
+	int flag = 1;
+	int flag_finish_find_current_min_distation_in_sigment_of_rank = 1;
+
 	pair<int, int> current_vertex;
 	pair<int, int> current_vertex_with_current_min_destation;
+
 	MPI_Status st;
-	if (rank == 0) 
-	{
-		MPI_Scatterv(graph, count_element_graph, dist_graph, MPI_INT, part_graph, count_element_graph[rank], MPI_INT, 0, MPI_COMM_WORLD); 
+	if (rank == 0) {
+		MPI_Scatterv(graph, count_element_graph, dist_graph, MPI_INT, part_graph, count_element_graph[rank], MPI_INT, 0, MPI_COMM_WORLD); //send graph
 		d = init_d(count_vertex);
 		d[start] = 0;
 		priority_queue<pair<int, int>>  queue;
 		queue.push(make_pair(0, start));
 
 		int flag_rank_0 = 1;
-		while (!queue.empty())
-		{
+		while (!queue.empty()) {
+
 			int v = queue.top().second;
 			int	dest = queue.top().first;
 			queue.pop();
@@ -163,19 +148,16 @@ int* parallel_dijkstra(int* graph, int start, int count_vertex)
 				continue;
 			MPI_Bcast(&flag, 1, MPI_INT, 0, MPI_COMM_WORLD);
 			current_vertex = make_pair(dest, v);
-			MPI_Bcast(&current_vertex, 1, MPI_2INT, 0, MPI_COMM_WORLD); 
+			MPI_Bcast(&current_vertex, 1, MPI_2INT, 0, MPI_COMM_WORLD); //send vertex and dist
 			MPI_Scatterv(d, count_element_d, dist_d, MPI_INT, part_d, count_element_d[rank], MPI_INT, 0, MPI_COMM_WORLD);
-			for (int i = 1; i < proc_num; i++)
-			{
+		
+			for (int i = 1; i < proc_num; i++) {
 				flag_rank_0 = 1;
-				while (flag_rank_0 != 0)
-				{
-					//std::cout << "wait" << std::endl;
+				while (flag_rank_0 != 0) {
+
 					MPI_Recv(&flag_finish_find_current_min_distation_in_sigment_of_rank, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &st);
-					//std::cout << "get" << std::endl;
 					flag_rank_0 = flag_finish_find_current_min_distation_in_sigment_of_rank;
-					if (flag_rank_0 == 0) 
-					{
+					if (flag_rank_0 == 0) {
 						continue;
 					}
 					MPI_Recv(&current_vertex_with_current_min_destation, 1, MPI_2INT, i, 0, MPI_COMM_WORLD, &st);
@@ -189,30 +171,22 @@ int* parallel_dijkstra(int* graph, int start, int count_vertex)
 		flag = 0;
 		MPI_Bcast(&flag, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	}
-	else
-	{
-		MPI_Scatterv(graph, count_element_graph, dist_graph, MPI_INT, part_graph, count_element_graph[rank], MPI_INT, 0, MPI_COMM_WORLD);
-
-		while (flag)
-		{
-			//std::cout << "succ" <<rank<< std::endl;
+	else {
+		MPI_Scatterv(graph, count_element_graph, dist_graph, MPI_INT, part_graph, count_element_graph[rank], MPI_INT, 0, MPI_COMM_WORLD); 
+		while (flag) {
 			MPI_Bcast(&flag, 1, MPI_INT, 0, MPI_COMM_WORLD);
-			if (flag == 0) 
-			{
+			if (flag == 0) {
 				continue;
 			}
-			MPI_Bcast(&current_vertex, 1, MPI_2INT, 0, MPI_COMM_WORLD);
+			MPI_Bcast(&current_vertex, 1, MPI_2INT, 0, MPI_COMM_WORLD); 
 			MPI_Scatterv(d, count_element_d, dist_d, MPI_INT, part_d, count_element_d[rank], MPI_INT, 0, MPI_COMM_WORLD);
 
-			for (int j = 0; j < count_element_d[rank]; j++) 
-			{
+			for (int j = 0; j < count_element_d[rank]; j++) {
 
 				int to = j, len = part_graph[j * count_vertex + current_vertex.second];
 
-				if (current_vertex.first + len < part_d[to])
-				{
+				if (current_vertex.first + len < part_d[to]) {
 					flag_finish_find_current_min_distation_in_sigment_of_rank = 1;
-					//std::cout << "send" << std::endl;
 					MPI_Send(&flag_finish_find_current_min_distation_in_sigment_of_rank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
 					current_vertex_with_current_min_destation.first = current_vertex.first + len;
 					current_vertex_with_current_min_destation.second = to + dist_d[rank];
@@ -227,8 +201,21 @@ int* parallel_dijkstra(int* graph, int start, int count_vertex)
 	}
 	return d;
 }
-int main(int argc, char *argv[])
-{
+
+bool is_correct_implementation(int* d1, int* d2, int size) {
+	int count_mistakes = 0;
+	for (int i = 0; i < size; i++) {
+		if (d1[i] != d2[i]) {
+			count_mistakes++;
+			return false;
+		}
+	}
+	if (count_mistakes == 0) {
+		return true;
+	}
+}
+
+int main(int argc, char *argv[]) {
 	int proc_num, proc_rank;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &proc_num);
@@ -241,26 +228,40 @@ int main(int argc, char *argv[])
 	int* d_step = nullptr;
 	int* d_parallel = nullptr;
 	int* graph = nullptr;
+
 	double t1_step = 0.0, t2_step = 0.0, t1_parallel = 0.0, t2_parallel = 0.0;
-	int start = rand() % (count_vertex - 1);
-	if (proc_rank == 0) 
-	{
+
+	int start;
+	if (proc_rank == 0) {
+		start = rand() % (count_vertex - 1);
+	}
+
+	if (proc_rank == 0) {
 		graph = init_graph(count_edge, count_vertex);
 	}
-	if (proc_rank == 0)
-	{
+	MPI_Barrier(MPI_COMM_WORLD);
+	if (proc_rank == 0) {
 		t1_parallel = MPI_Wtime();
 	}
+	
 	d_parallel = parallel_dijkstra(graph, start, count_vertex);
 	MPI_Barrier(MPI_COMM_WORLD);
-	if (proc_rank == 0) 
-	{
+	if (proc_rank == 0) {
 		t2_parallel = MPI_Wtime();
+
 		cout << endl << " Parallel Dijkstra time: " << t2_parallel - t1_parallel << endl << endl;
+
+		cout << endl << endl;*/
 		t1_step = MPI_Wtime();
 		d_step = dijkstra(graph, start, count_vertex);
 		t2_step = MPI_Wtime();
 		cout << endl << " Dijkstra time: " << t2_step - t1_step << endl << endl;
+		if (is_correct_implementation(d_step, d_parallel, count_vertex)) {
+		cout << "parallel implementayion is correct" << endl;
+		}
+		else {
+		cout << "parallel implementayion isn`t correct" << endl;
+		}
 	}
 	MPI_Finalize();
 }
